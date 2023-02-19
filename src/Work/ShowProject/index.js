4b5f0cd6550278be6cb8aabdc774b62b7b09646a
Kelvin Arias <kelvin727631@gmail.com>
2023-02-18 23:08:40 -0500

    creating slider for proyects

--- src/Work/ShowProject/index.js
+++ src/Work/ShowProject/index.js
@@ -1,13 +1,60 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
+import cx from "classnames";
 import "./styles.css";
 
 const ShowProject = ({ selectedProjectInfo, onClose }) => {
+  const [activeImage, setActiveImage] = useState(0);
+  const imagesPoint = [];
   const { companyName, images, date, position, description, skills, logo } =
     selectedProjectInfo;
+
+  const handleScroll = (event) => {
+    const delta = event.deltaY;
+    const numImages = images.length;
+    setTimeout(() => {
+      setActiveImage(
+        (activeImage + numImages + (delta > 0 ? 1 : -1)) % numImages
+      );
+    }, 500);
+  };
+
   return (
-    <div className="projectModal" onClick={onClose}>
+    <div className="projectModal" onClick={onClose} onWheel={handleScroll}>
       <div className="projectContent" onClick={(e) => e.stopPropagation()}>
-        <img className="projectImg" src={images[0].src} alt={images[0].alt} />
+        <div className="imgContainer">
+          {images.map((img, index) => {
+            imagesPoint.push(
+              <div
+                key={index}
+                className={cx("projectPoint", {
+                  active: activeImage === index,
+                })}
+                onClick={() => setActiveImage(index)}
+              />
+            );
+            return (
+              <img
+                className={cx("projectImage", {
+                  previous:
+                    activeImage - 1 === index ||
+                    (activeImage === 0 && index === images.length),
+                  active: activeImage === index,
+                  next: activeImage + 1 === index,
+                })}
+                key={img.alt}
+                src={img.src}
+                alt={img.alt}
+              />
+            );
+          })}
+          <div
+            className="pointsContainer"
+            style={{ height: `${images.length * 20 + 20}px` }}
+          >
+            {imagesPoint}
+          </div>
+        </div>
         <h2>{companyName}</h2>
         <div className="projectInformation">
           <ul className="projectList">
